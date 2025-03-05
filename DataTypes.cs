718da6a5741992715e3684a4ee8a5518a442c35e
Roman Pirshtuk <88338997+pirsztuk@users.noreply.github.com>
2025-03-05 20:41:31 +0300

    Data Types (from level 2 unit 2)

---
+++ DataTypes.cs
@@ -0,0 +1,36 @@
+// Integer number
+
+int integerValue = 1;
+
+
+// Fractional number (from less precise to more precise)
+
+// To set a float literal we use F
+float floatValue = 1.25F; // 6-9 digits after a comma
+
+
+// To create a double literal, just enter a decimal number.  No suffix is needed
+double doubleValue = 1.25; // 15-17 digits after a comma
+
+
+// To set a decimal literal we use M
+decimal decimalValue = 1.25M; // 28-29 digits after a comma
+
+
+// String and char
+
+string stringValue = "A value with a text"; // Double quotes ESSENTIAL
+
+char charValue = 'H'; // Single quotes ESSENTIAL and only one char inside
+
+
+// Boolean type
+
+bool booleanValue = true; // true/false
+
+
+// From Microsoft Learn: 
+// Suppose you needed to collect data from a user, like a phone number or postal code. 
+// Depending on the country/region where you live, that data may consist of numeric characters. 
+// However, since you rarely perform mathematical calculations on phone numbers and 
+// postal codes, you should prefer to use a string data type when working with them.
